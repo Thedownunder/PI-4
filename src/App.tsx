1ce0989be57be979ee81777db40737942e2d7ad8
CarlimZeroBala <102628951+CarlimZeroBala@users.noreply.github.com>
2025-11-27 21:16:21 -0300

    Delete src directory

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import Board from "./pages/board";
-
-export default function App() {
-  return <Board onLogout={() => { /* plugue sua lógica aqui */ }} />;
-}
--- src/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Configuração padrão para React + JSX automático
-export default defineConfig({
-  plugins: [react()],
-});
